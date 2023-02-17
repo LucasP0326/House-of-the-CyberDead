abda8df2ee598930feb58b0e9216528e03370ec3
Anniken20 <111744298+Anniken20@users.noreply.github.com>
2023-02-16 20:27:21 -0500

    SCRIPTS AND MODELS
    Door script, keycard functioning with E for pick up, models available

---
+++ Assets/Open.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Open : MonoBehaviour
+{
+    public Animation hinge;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnTriggerStay()
+    {
+        if (Input.GetKey (KeyCode.E))
+        hinge.Play ();
+    }
+}
