02cf7bb0803c7035962e27665dbb7d133c184ebf
Anniken20 <111744298+Anniken20@users.noreply.github.com>
2023-02-24 08:24:54 -0500

    Replay and or get lost bucko

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -64,5 +65,7 @@ public class PlayerMovement : MonoBehaviour
 
     public void Death(){
         Debug.Log("You died!");
+
+              SceneManager.LoadScene("Lose", LoadSceneMode.Additive);
     }
 }
